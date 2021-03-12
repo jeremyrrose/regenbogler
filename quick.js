5a7eddd60daa598bd92259e0798f3900fdc9dca0
jeremyrrose <jeremy.r.rose@gmail.com>
2021-03-12 02:04:05 -0500

    better quickSort

--- quick.js
+++ quick.js
@@ -3,7 +3,7 @@ const message =
 "\033[2J\nin 25 werds or less:" +
 "\nwhy is this so slo?" +
 "\n\n\n"
-const timeout = 10
+const timeout = 100
 
 const numsArr = [ 1,1,1, 6, 1,1,1,1834, 2, 4, 7, 12, 1, 5, 3, 66, 12, 7, 3 ]
 const alphaArr = "abcdefghijklmnopqrstuvwxyz".split('')
@@ -16,10 +16,10 @@ let comparisons = 0
 
 const quick = async (arr, bow) => {
 
-    splits++
     if (arr.length < 2) {
         return arr
     }
+    splits++
 
     await new Promise(resolve => setTimeout(resolve, timeout))
     console.log(bow.print(arr, false, `\n\ntotal steps: ${comparisons}\nsplits ${splits}`))
@@ -45,9 +45,9 @@ const quick = async (arr, bow) => {
     await new Promise(resolve => setTimeout(resolve, timeout))
     console.log(bow.print([...left, "||", pivot, "||", ...right], false, `\n\ntotal steps: ${comparisons}\nsplits ${splits}`))
     
-    const result = [...await quick(left, bow), pivot, ...await quick(right, bow)]
-    console.log(bow.print(result, false, `\n\ntotal steps: ${comparisons}\nsplits ${splits}`))
-    return result
+    const result = [await quick(left, bow), pivot, await quick(right, bow)]
+    console.log(bow.print([...result[0],"||",pivot,"||",...result[2]], false, `\n\ntotal steps: ${comparisons}\nsplits ${splits}`))
+    return [...result[0], pivot, ...result[2]]
 
 }
 
