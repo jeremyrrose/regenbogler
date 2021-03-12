9c5dda834b5ca876c88612c424023fb8166a6053
jeremyrrose <jeremy.r.rose@gmail.com>
2021-03-11 22:30:53 -0500

    overdue initial commit

---
+++ bubble.js
@@ -0,0 +1,47 @@
+const Regenbogler = require('./index.js')
+const message =  
+"\033[2J\nin 25 werds or less:" +
+"\nwhy is this so slo?" +
+"\n\n\n"
+
+
+const numsArr = [ 1,1,1, 6, 1,1,1,1834, 2, 4, 7, 12, 1, 5, 3, 66, 12, 7, 3 ]
+const alphaArr = "abcdefghijklmnopqrstuvwxyz".split('')
+const sentenceArr = "the quick brown fox jumps over the lazy dog".split(" ")
+
+const bubbler = async arr => {
+
+    const bow = new Regenbogler(arr, true, message)
+
+    let ops = 0
+
+    for (let i = 0; i < arr.length - 1; i++) {
+
+        let flip = false
+
+        for (let j=0; j < arr.length - 1; j++) {
+            
+            await new Promise(resolve => setTimeout(resolve, 200))
+            console.log(bow.print(arr, false, `\n\ntotal steps: ${ops}\npass ${i+1}; step ${j+1}`, j))
+            // console.log(bow.print(arr, false, false, j))
+            // console.log(bow.string(arr, j))
+            // console.log(bow.string(arr, j), "\n" + bow.base(), "\n" + bow.target())
+
+            if (arr[j] > arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+                flip = true
+            }
+
+            ops++
+        }
+
+        // if (!flip) {
+        //     return arr
+        // }
+    }
+
+    return arr
+}
+
+bubbler([...alphaArr].sort(() => Math.random() - .5))
+// bubbler(sentenceArr)
---
+++ index.js
@@ -0,0 +1,57 @@
+class Regenbogler {
+    constructor(arr, sort=true, message="", vals=[96,160,192,255]) {
+
+        const rgbs = [ ]
+        for (let val1 of vals) {
+            for (let val2 of vals) {
+                for (let val3 of vals) {
+                    if (val1 && val2 || val1 && val3 || val2 && val3){
+                        rgbs.push([val1,val2,val3])
+                    }
+                }
+            }
+        }
+
+        this.arr = arr
+        this.orig = [...arr]
+        this.sorted = sort? [...arr].sort() : arr
+        this.colors = {}
+        arr.forEach((item, i) => {
+            this.colors[item] = this.colors[item] ? this.colors[item] : rgbs[i < rgbs.length ? rgbs.length - 1 - i : i % rgbs.length]
+        })
+        this.message = message
+    }
+
+    print(arr, pre=null, post=null, index=null) {
+        let str = this.message + (pre ? pre : '')
+        str += this.base() + "\n\n"
+        str += this.string(arr, index) + "\n\n"
+        str += this.target() + "\n\n"
+        str += post ? post : ""
+        return str
+    }
+
+    string(arr, index=null) {
+        let str = ""
+        arr.forEach((item, i) => {
+            // bold and underline
+            str += i == index ? "\x1b[1m\x1b[4m" : ""
+            str += ((r,g,b)=>`\x1b[38;2;${r};${g};${b}m`)(...this.colors[item])
+            str += item       
+            // remove bold and underline      
+            str += i == index ? "\x1b[22m\x1b[24m " : " "
+        })
+        // console.log(str)
+        return str
+    }
+
+    base() {
+        return this.string(this.orig)
+    }
+
+    target() {
+        return this.string(this.sorted)
+    }
+}
+
+module.exports = Regenbogler;
