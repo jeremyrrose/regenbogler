7b86d443e97bbbacdaf8494d37da3653c35cb7a1
jeremyrrose <jeremy.r.rose@gmail.com>
2021-03-15 13:33:14 -0400

    output commenting on merge

--- merge.js
+++ merge.js
@@ -4,14 +4,17 @@ const message =
 "\033[2J\nM E R G E  S O R T:" +
 "\n\n\n"
 
+// for output
 let splits = 0
 let merges = 0
 let ops = 0
+// end output
 
 
 const mergeSort = async (arr, bow) => {
 
-    splits++
+    splits++ // for output
+
     if (arr.length < 2) {
         return arr
     }
@@ -21,20 +24,23 @@ const mergeSort = async (arr, bow) => {
     const left = arr.slice(0,center)
     const right = arr.slice(center)
 
-    ops++
+    // for output
+    ops++ 
     await new Promise(resolve => setTimeout(resolve, timeout))
     if (!wave) {
         console.log(bow.print([...left, "||", ...right], false, `\n\ntotal steps: ${ops}\nsplits ${splits}; merges ${merges}`))
     } else {
         console.log(bow.string([...left, "||", ...right]))
     }
+    // end output
 
     return await merge(await mergeSort(left, bow), await mergeSort(right, bow), bow)
 
 }
 
 const merge = async (left, right, bow) => {
-    merges++
+    merges++ // for output
+
     const result = []
     while (left.length && right.length) {
         if (left[0] < right[0]) {
@@ -42,6 +48,8 @@ const merge = async (left, right, bow) => {
         } else {
             result.push(right.shift())
         }
+
+        // for output
         ops++
         await new Promise(resolve => setTimeout(resolve, timeout))
         if (!wave) {
@@ -49,9 +57,10 @@ const merge = async (left, right, bow) => {
         } else {
             console.log(bow.string([...result, "<<", ...left, "||", ...right]))
         }
+        // end output
     }
     return [...result, ...left, ...right]
 }
 
 const bow = new Regenbogler(alphaArr.sort(()=>Math.random() - .5), true, message)
-console.log(mergeSort(bow.orig, bow))
+mergeSort(bow.orig, bow)
